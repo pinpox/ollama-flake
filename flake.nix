f2c8be22d1d1a4183341fa963ef4d361e3a7f891
pinpox <git@pablo.tools>
2025-06-11 00:05:00 +0200

    simplify

--- flake.nix
+++ flake.nix
@@ -14,58 +14,35 @@
         pkgs = import nixpkgs { inherit system; };
         modelFile = pkgs.fetchurl {
 
-          # url= "https://huggingface.co/gaebalai/DeepSeek-R1-0528-Qwen3-8B-Q8-GUFF/resolve/main/DeepSeek-R1-0528-Qwen3-8B-Q8_0.gguf";
-          # sha256 = "sha256-emsr08k618afIAF8AZ5sOKOYh5aNsdjgAPKt25Nryc0=";
+          url = "https://huggingface.co/Qwen/Qwen2.5-Coder-3B-Instruct-GGUF/resolve/main/qwen2.5-coder-3b-instruct-q2_k.gguf";
+          sha256 = "sha256-z1hiYV8Q59GRMeyMMiPFGsQ7Y6ZqBge0TbK2lDFE1gc=";
 
-          url = "https://huggingface.co/h4shy/gemma-3-1b-it-fast-GUFF/resolve/main/gemma3-1b-Q8_0.gguf";
-          sha256 = "sha256-z3V0tROK77VOmnDCgUSnDcvr27afwKlVj56zUC1O9R8=";
         };
       in
       {
+        packages.default = pkgs.writeShellApplication {
 
-        packages = rec {
+          name = "offline-llm";
 
-          model = pkgs.stdenv.mkDerivation {
-            name = "modelfile";
+          text = ''
 
-            src = pkgs.fetchgit {
-              url = "https://huggingface.co/Qwen/Qwen2.5-Coder-3B";
-              rev = "09d9bc5d376b0cfa0100a0694ea7de7232525803";
-              fetchLFS = true;
-              sha256 = "sha256-VSaxtj1WZB6upGCbqZThamlkYIAjrzkMxnnrXy8JUyg=";
-            };
+            if [ "$#" -eq 0 ]; then
+              echo "Usage: $0 'your prompt'"
+              exit 1
+            fi
 
-            buildInputs = [
-              pkgs.llama-cpp
-              pkgs.python3
-            ];
+            PROMPT="$1"
+            # shellcheck disable=SC2016
+            ${pkgs.llama-cpp}/bin/llama-cli -m ${modelFile} \
+            -no-cnv \
+            --offline \
+            --no-warmup \
+            --no-display-prompt \
+            --jinja -fa \
+            -p "$PROMPT" 2>/dev/null | \
+            awk 'BEGIN{p=0} /^```/{p=!p; next} p{print}'
 
-            buildPhase = "convert_hf_to_gguf.py $src --outfile model.gguf";
-            installPhase = "cp model.gguf $out";
-          };
-
-          default = pkgs.writeShellApplication {
-
-            name = "offline-llm";
-
-            text = ''
-
-              if [ "$#" -eq 0 ]; then
-                echo "Usage: $0 'your prompt'"
-                exit 1
-              fi
-
-              PROMPT="$1"
-              # shellcheck disable=SC2016
-              ${pkgs.llama-cpp}/bin/llama-cli -m ${model} \
-              -no-cnv \
-              --offline \
-              --no-warmup \
-              --no-display-prompt \
-              -p "$PROMPT"
-
-            '';
-          };
+          '';
         };
       }
     );
